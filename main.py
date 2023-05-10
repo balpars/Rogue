8e3a823667ba5bd6c57ad7a252c9d95e57c83c9f
Bedirhan Alp Arslan <89816332+balpars@users.noreply.github.com>
2023-05-10 22:06:34 +0300

    added enemis

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tcod
 
+import copy
 from engine import Engine
 from input_handlers import EventHandler
-from entity import Entity
+import entity_factories
 from procgen import generate_dungeon
 
 def main() -> None:
@@ -15,14 +16,15 @@ def main() -> None:
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
+    max_monsters_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-    entities = {npc, player}
+    player = copy.deepcopy(entity_factories.player)
+
+
     event_handler = EventHandler()
 
     game_map = generate_dungeon(
@@ -31,9 +33,10 @@ def main() -> None:
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
+        max_monsters_per_room=max_monsters_per_room,
         player=player
     )
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
